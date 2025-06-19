d62b9237a9e0e29b62d22b3fa699c24d754367bd
thiendoan1208 <thienmacro1234@gmail.com>
2025-06-19 15:18:40 -0400

    check user permision

--- src/services/axios-config.tsx
+++ src/services/axios-config.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "sonner";
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL,
@@ -28,7 +29,46 @@ instance.interceptors.response.use(
   function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    return Promise.reject(error);
+    const status = (error && error.response && error.response.status) || 500;
+    switch (status) {
+      // authentication (token related issues)
+      case 401: {
+        toast.error("Unauthorized");
+        // window.location.href = "/login";
+        return Promise.reject(error);
+      }
+
+      // forbidden (permission related issues)
+      case 403: {
+        toast.error("No permission");
+        return Promise.reject(error);
+      }
+
+      // bad request
+      case 400: {
+        return Promise.reject(error);
+      }
+
+      // not found
+      case 404: {
+        return Promise.reject(error);
+      }
+
+      // conflict
+      case 409: {
+        return Promise.reject(error);
+      }
+
+      // unprocessable
+      case 422: {
+        return Promise.reject(error);
+      }
+
+      // generic api error (server related) unexpected
+      default: {
+        return Promise.reject(error);
+      }
+    }
   }
 );
 
